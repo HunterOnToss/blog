cf2550adc7b2a6ba1a5091c4d613c74b8dc7bb03
hunter <stasdolg@mail.ru>
2015-07-12 05:20:52 +0600

    добавил модель статьи

--- src/article/models.py
+++ src/article/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    class Meta():
+        db_table = "_article"
+
+    article_title = models.CharField(max_length=150)
+    article_text = models.TextField()
+    article_data = models.DateTimeField()
+    article_likes = models.IntegerField()
