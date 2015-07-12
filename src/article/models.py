e655a70dbd7707821f08c219a5b16a1adbf71f34
hunter <stasdolg@mail.ru>
2015-07-12 06:09:27 +0600

    add admin

--- src/article/models.py
+++ src/article/models.py
@@ -1,13 +1,12 @@
 from django.db import models
 
-
 class Article(models.Model):
     class Meta:
         db_table = "article"
 
     article_title = models.CharField(max_length=150)
     article_text = models.TextField()
-    article_data = models.DateTimeField()
+    article_date = models.DateTimeField()
     article_likes = models.IntegerField(default=0)
 
 
