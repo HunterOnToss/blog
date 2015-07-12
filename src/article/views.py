c5813a55fea211309f525294f730974ed2c7ae79
hunter <stasdolg@mail.ru>
2015-07-12 06:44:23 +0600

    первый способ рендерить вьюху

--- src/article/views.py
+++ src/article/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http.response import HttpResponse
 
-# Create your views here.
+
+def basic_one(request):
+    view = "basic_one"
+    html = "<html><body>this is %s view </body><html>" % view
+    return HttpResponse(html)
