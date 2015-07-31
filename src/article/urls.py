b06fd3733c13688412b6aeebbaf46b7511f7c302
hunter <stasdolg@mail.ru>
2015-08-01 01:09:17 +0600

    Добавил повышения like при нажатие на него

--- src/article/urls.py
+++ src/article/urls.py
@@ -6,6 +6,7 @@ urlpatterns = patterns('',
                        url(r'^3/', 'article.views.template_three'),
                        url(r'^articles/all/$', 'article.views.articles'),
                        url(r'^articles/get/(?P<article_id>\d+)/$', 'article.views.article'),
+                       url(r'^articles/add_like/(?P<article_id>\d+)/$', 'article.views.add_like'),
 
                        url(r'^$', 'article.views.articles'),
                        )
