6bbd4b049749fd96537a6c9b7ac4fd3302c03b11
hunter <stasdolg@mail.ru>
2015-08-03 22:18:17 +0600

    Добавление пользователей

--- src/login_sys/urls.py
+++ src/login_sys/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
                        url(r'^login/', 'login_sys.views.login'),
                        url(r'^logout/', 'login_sys.views.logout'),
+                       url(r'^register/', 'login_sys.views.register'),
                        )
