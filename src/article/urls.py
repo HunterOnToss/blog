e2d746a01ab14e71dfbfaab06711feb06775a38f
hunter <stasgold@mail.ru>
2015-10-18 00:27:08 +0600

    Доудалял лайк, изменил стил

--- src/article/urls.py
+++ src/article/urls.py
@@ -5,9 +5,7 @@ urlpatterns = patterns('',
                        url(r'^2/', 'article.views.template_two'),
                        url(r'^articles/all/$', 'article.views.articles'),
                        url(r'^articles/get/(?P<article_id>\d+)/$', 'article.views.article'),
-                       url(r'^articles/add_like/(?P<article_id>\d+)/$', 'article.views.add_like'),
                        url(r'^articles/add_comment/(?P<article_id>\d+)/$', 'article.views.add_comment'),
                        url(r'^page/(\d+)/$', 'article.views.articles'),
                        url(r'^$', 'article.views.articles'),
-
                        )
