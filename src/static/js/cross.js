a93e535ec410ef8a5ca96cf93ada0dac1a875c5a
Stanislav <hunter@Mac-mini-Stanislav.local>
2015-12-01 19:14:47 +0300

    При нажатие на иконку,появляется черный экран, как будто запускается приложение на  Ipad

--- src/static/js/cross.js
+++ src/static/js/cross.js
@@ -1,12 +1,16 @@
 $(document).ready(function(){
     $(".icon").click(function(){
-        var back = $(this).css("background");
-        PopUpShow(back);
+        var container = $(".container");
+        var height_container = container.css("height");
+        var width_container = container.css("width");
+        var top_container = container.css("top");
+        var left_container = container.css("left");
+        PopUpShow(height_container, width_container, top_container, left_container);
     });
 });
 
-function PopUpShow(image){
-    $("#popup1").show().css('background', image);
+function PopUpShow(height_container, width_container, top_container, left_container){
+    $("#popup1").show().css('background-color', "black").css("height", height_container).css("width", width_container).css("top", top_container).css("left", left_container);
 }
 
 function PopUpHide(){
