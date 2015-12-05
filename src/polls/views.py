1103c193070fa2c2c86db6fc97775dcd8648c2a9
hunter <stasgold@mail.ru>
2015-12-05 09:31:51 +0600

    Ненужная view

--- src/polls/views.py
+++ src/polls/views.py
@@ -30,15 +30,6 @@ class IndexView(AJAXListMixin, generic.ListView):
         return Poll.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 
-#
-# class DetailView(generic.DetailView):
-#     model = Poll
-#     template_name = "polls/detail.html"
-#
-#     def get_queryset(self):
-#         return Poll.objects.filter(pub_date__lte=timezone.now())
-
-
 class ResultsView(generic.DetailView):
     model = Poll
     template_name = "polls/result.html"
