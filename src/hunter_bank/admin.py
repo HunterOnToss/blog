484dec9e373fb212514cb7b066d35257dbde3691
hunter <stasdolg@mail.ru>
2016-01-09 22:28:46 +0600

    Добавил в админку Offer и Oraganization

--- src/hunter_bank/admin.py
+++ src/hunter_bank/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from hunter_bank.models import Offer, Organization
 
-# Register your models here.
+
+class OfferAdmin(admin.ModelAdmin):
+    list_display = ('offer_create', 'offer_update', 'offer_rotation_begin')
+
+
+class OrganizationAdminAdmin(admin.ModelAdmin):
+    list_display = ('organization_name', 'organization_status')
+
+
+admin.site.register(Offer, OfferAdmin)
+admin.site.register(Organization, OrganizationAdminAdmin)
