d792150a019931b63fbde65858e862d975dd3e6a
hunter <stasdolg@mail.ru>
2016-01-10 22:10:09 +0600

    Добавил Админку для клиента

--- src/hunter_bank/admin.py
+++ src/hunter_bank/admin.py
@@ -1,14 +1,20 @@
 from django.contrib import admin
-from hunter_bank.models import Offer, Organization
+from hunter_bank.models import Offer, Organization, Client
 
 
 class OfferAdmin(admin.ModelAdmin):
     list_display = ('offer_create', 'offer_update', 'offer_rotation_begin')
 
 
-class OrganizationAdminAdmin(admin.ModelAdmin):
+class OrganizationAdmin(admin.ModelAdmin):
     list_display = ('organization_name', 'organization_status')
 
 
+class ClientAdmin(admin.ModelAdmin):
+    list_filter = ['client_create']
+    list_display = ('pk', 'client_create', 'client_update', 'client_name', 'client_family', 'client_otchestvo',
+                    'client_birthday', 'client_phone_number', 'client_passport_number', 'client_scoring_point')
+
 admin.site.register(Offer, OfferAdmin)
-admin.site.register(Organization, OrganizationAdminAdmin)
+admin.site.register(Organization, OrganizationAdmin)
+admin.site.register(Client, ClientAdmin)
