189b872ccffe2bec9a2801a2926f40b80b7a3525
hunter_mac_os_x <stasdolg@mail.ru>
2016-01-12 12:41:39 +0300

    Endpoint for the highlighted snippets, and for the root of our API

--- src/snippets/urls.py
+++ src/snippets/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     url(r'^users/$', views.UserList.as_view()),
     url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^snippets/(?P<pk>[0-9]+)/highlight/$', views.SnippetHighlight.as_view()),
+    url(r'^api-root/$', views.api_root),
 ]
 
 # http://127.0.0.1:8000/snippets.json vs http://127.0.0.1:8000/snippets.api
